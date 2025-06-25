7fd52a9fe5781bc2dcd88065543738b8e32bb75a
Anthony Paulo <apauloprod@gmail.com>
2025-06-25 07:30:34 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,11 @@
 <!-- social_site/db.php -->
 <?php
 $host = 'localhost';
+$port = "5432";
 $db = 'socialsite';
 $user = 'postgres';
 $pass = 'Visionary88!';
-$conn = pg_connect("host=$host dbname=$db user=$user password=$pass");
+$conn = pg_connect("host=$host port=$port dbname=$db user=$user password=$pass");
 if (!$conn) {
     die("Connection failed: " . pg_last_error());
 }
