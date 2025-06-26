03989d27c7aca37734dde019d70b4e14a477e84c
Anthony Paulo <apauloprod@gmail.com>
2025-06-25 20:38:53 -0400

    v2

--- home.php
+++ home.php
@@ -14,20 +14,32 @@ $user_id = $_SESSION['user_id'];
 <!DOCTYPE html>
 <html>
 <head>
-    <title>LYV</title>
+    <title>Home - LYV</title>
     <link rel="stylesheet" href="futuristic_theme.css">
+    <style>
+        .top-right {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+        }
+        .top-right a {
+            margin: 0 5px;
+        }
+    </style>
 </head>
 <body>
+    <div class="top-right">
+        <?php if (isset($_SESSION['user_id'])): ?>
+            Logged in as <strong><?= htmlspecialchars($_SESSION['username'] ?? 'User') ?></strong> | <a href="logout.php">Logout</a>
+        <?php else: ?>
+            <a href="signup.php">Sign Up</a> | <a href="login.php">Login</a>
+        <?php endif; ?>
+    </div>
+
     <h1>Live Your Vision</h1>
-    <a href="signup.php">Sign Up</a> | <a href="login.php">Login</a>
+    <a href="profile.php">Edit Profile</a> | <a href="post.php">New Post</a> | <a href="feed.php">Community Board</a> | <a href="spaceminigame.php">Mini Game</a>
 </body>
 </html>
 
-
-
-
-
-<h1>Welcome!</h1>
-<a href="profile.html">Edit Profile</a> | <a href="post.php">New Post</a> | <a href="feed.html">Community Board</a> | <a href="logout.php">Logout</a> | <a href="spaceminigame.php">Mini Game</a>
 
 
--- index.php
+++ index.php
@@ -14,13 +14,28 @@ if (isset($_SESSION['user_id'])) {
 <head>
     <title>LYV</title>
     <link rel="stylesheet" href="futuristic_theme.css">
+    <style>
+        .top-right {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+        }
+        .top-right a {
+            margin: 0 5px;
+        }
+    </style>
 </head>
 <body>
+    <div class="top-right">
+        <?php if (isset($_SESSION['user_id'])): ?>
+            Logged in as <strong><?= htmlspecialchars($_SESSION['username'] ?? 'User') ?></strong>
+        <?php else: ?>
+            <a href="signup.php">Sign Up</a> | <a href="login.php">Login</a>
+        <?php endif; ?>
+    </div>
     <h1>Live Your Vision</h1>
-    <a href="signup.php">Sign Up</a> | <a href="login.php">Login</a>
 </body>
 </html>
-
 
 
 
